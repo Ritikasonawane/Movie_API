4e664e53011761e1b3ef79edf8644a3bfd182db0
Ritikasonawane <ritika.sonawane2003@gmail.com>
2024-01-17 03:29:21 -0800

    Refactor

---
+++ src/main/java/com/ritika/demo/controller/Controller.java
@@ -0,0 +1,38 @@
+package com.ritika.demo.controller;
+
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import com.ritika.demo.dto.DiscoverResponse;
+import com.ritika.demo.service.MovieService;
+
+@RestController
+@RequestMapping("/movies")
+public class Controller {
+	Logger logger = LoggerFactory.getLogger(Controller.class);
+	
+	@Autowired
+	MovieService movieServiceImpl;
+
+	@RequestMapping(value = "/byYearSearch", method = RequestMethod.GET)
+	public DiscoverResponse getMovieByYear(@RequestParam Integer movieYear) {
+		DiscoverResponse discoverResponseBean;
+	    discoverResponseBean = movieServiceImpl.fetchDiscoverDetailsByYear(movieYear);
+		return discoverResponseBean;
+
+	}
+
+	@GetMapping("/topRatedMovies")
+	public DiscoverResponse getTopRatedMovies() {
+		DiscoverResponse discoverResponseBean;
+		discoverResponseBean = movieServiceImpl.fetchTopRatedMovies();
+		return discoverResponseBean;
+
+	}
+
+}
