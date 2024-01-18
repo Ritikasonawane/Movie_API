2ad0a8c4e392e7d32e9422b82a9adb852ec4231b
Ritikasonawane <ritika.sonawane2003@gmail.com>
2024-01-18 00:29:07 -0800

    Refactor: uses class-level initialization.
    Instead of Declaring in each method, declared it at class-level.

--- src/main/java/com/ritika/demo/service/imp/MovieServiceImplementation.java
+++ src/main/java/com/ritika/demo/service/imp/MovieServiceImplementation.java
@@ -1,61 +1,60 @@
 package com.ritika.demo.service.imp;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
-
-import com.ritika.demo.dto.DiscoverResponse;
-import com.ritika.demo.exception.MoviNotFoundException;
+import com.ritika.demo.dto.DiscoveryResponse;
+import com.ritika.demo.exception.MovieNotFoundException;
 import com.ritika.demo.moviecontroller.MovieController;
-import com.ritika.demo.repository.UserRepository;
 import com.ritika.demo.service.MovieService;
 
 @Service("MovieServiceImpl")
 public class MovieServiceImplementation implements MovieService {
 	Logger logger = LoggerFactory.getLogger(MovieController.class);
-	@Autowired
-	RestTemplate restTemplate;
+
+	static UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl("https://api.themoviedb.org/3/");
+
+	final RestTemplate restTemplate;
+
+	public MovieServiceImplementation(RestTemplate restTemplate) {
+		this.restTemplate = restTemplate;
+	}
 
 	@Override
-	public DiscoverResponse fetchDiscoverDetailsByYear(Integer movieYear) {
+	public DiscoveryResponse fetchDiscoverDetailsByYear(Integer movieYear) {
 
-		UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl("https://api.themoviedb.org/3/");
 		String primaryReleaseYear = builder.path("discover/movie")
 				.queryParam("api_key", "016fe97483a235a2c1b75bce3028f7b7").queryParam("primary_release_year", movieYear)
 				.build().toString();
 		System.out.println("primaryReleaseYear" + primaryReleaseYear);
-		DiscoverResponse responseBeanArray = null;
-		ResponseEntity<DiscoverResponse> movieResponseEntity;
+		DiscoveryResponse responseBeanArray = null;
+		ResponseEntity<DiscoveryResponse> movieResponseEntity;
 		try {
-			movieResponseEntity = restTemplate.getForEntity(primaryReleaseYear, DiscoverResponse.class);
+			movieResponseEntity = restTemplate.getForEntity(primaryReleaseYear, DiscoveryResponse.class);
 			responseBeanArray = movieResponseEntity.getBody();
 		} catch (RestClientException e) {
-			throw new MoviNotFoundException("We can't fetch movie for year " + movieYear);
+			throw new MovieNotFoundException("We can't fetch movie for year " + movieYear);
 
 		}
-		
-		  if(responseBeanArray == null) {
-			  throw new MoviNotFoundException("Movies couldn't be fetched for year " + movieYear); 
-			 }
-		 
+		if (responseBeanArray == null) {
+			throw new MovieNotFoundException("Movies couldn't be fetched for year " + movieYear);
+		}
+
 		return responseBeanArray;
 	}
 
 	@Override
-	public DiscoverResponse fetchTopRatedMovies() {
-		UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl("https://api.themoviedb.org/3/");
+	public DiscoveryResponse fetchTopRatedMovies() {
 		String topRatedMoviesUrl = builder.path("movie/top_rated")
 				.queryParam("api_key", "016fe97483a235a2c1b75bce3028f7b7").build().toString();
-		DiscoverResponse responseBeanArray = null;
+		DiscoveryResponse responseBeanArray = null;
 		try {
-			ResponseEntity<DiscoverResponse> movieResponseEntity = restTemplate.getForEntity(topRatedMoviesUrl,
-					DiscoverResponse.class);
+			ResponseEntity<DiscoveryResponse> movieResponseEntity = restTemplate.getForEntity(topRatedMoviesUrl,
+					DiscoveryResponse.class);
 			responseBeanArray = movieResponseEntity.getBody();
 		} catch (Exception e) {
 			logger.error(e.getMessage());
