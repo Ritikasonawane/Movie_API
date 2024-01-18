6e5d0c000ec6094eaf4e7b3811f21b24ee0b8a16
Ritikasonawane <ritika.sonawane2003@gmail.com>
2024-01-18 00:18:55 -0800

    Modify Exception handler.

--- src/main/java/com/ritika/demo/exception/CustomExceptionHandler.java
+++ src/main/java/com/ritika/demo/exception/CustomExceptionHandler.java
@@ -1,7 +1,6 @@
 package com.ritika.demo.exception;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -9,14 +8,13 @@ import org.springframework.web.context.request.WebRequest;
 
 @RestControllerAdvice
 public class CustomExceptionHandler {
-	@ExceptionHandler(MoviNotFoundException.class)
-	public ResponseEntity<String> handleMovieNotFoundException(MoviNotFoundException mnfe){
+	@ExceptionHandler(MovieNotFoundException.class)
+	public ResponseEntity<String> handleMovieNotFoundException(MovieNotFoundException mnfe){
 	return new ResponseEntity<String>(mnfe.getMessage(),HttpStatus.NOT_FOUND);
 	}
 	
 	@ExceptionHandler(Exception.class)
 	public ResponseEntity<String> handleAllException(Exception exception, WebRequest request){
 		return new ResponseEntity<String>(exception.getMessage(),HttpStatus.BAD_REQUEST);
-		
 	}
 }
